43766a847ecde40f26a6198161092866a0c2e1da
Lucas Santana <santanafx@hotmail.com>
2023-08-11 14:48:16 -0300

    finalizado conversão de prop drilling para redux

--- src/containers/Produtos.tsx
+++ src/containers/Produtos.tsx
@@ -1,21 +1,16 @@
+import { useSelector } from 'react-redux'
 import { Produto as ProdutoType } from '../App'
 import Produto from '../components/Produto'
+import { useGetProdutosQuery } from '../services/api'
 
 import * as S from './styles'
+import { RootReducer } from '../store'
 
-type Props = {
-  produtos: ProdutoType[]
-  favoritos: ProdutoType[]
-  adicionarAoCarrinho: (produto: ProdutoType) => void
-  favoritar: (produto: ProdutoType) => void
-}
+const ProdutosComponent = () => {
+  const { data: produtos, isLoading } = useGetProdutosQuery()
+  const favoritos = useSelector((state: RootReducer) => state.favoritos.itens)
 
-const ProdutosComponent = ({
-  produtos,
-  favoritos,
-  adicionarAoCarrinho,
-  favoritar
-}: Props) => {
+  if (isLoading) return <h2>Carregando...</h2>
   const produtoEstaNosFavoritos = (produto: ProdutoType) => {
     const produtoId = produto.id
     const IdsDosFavoritos = favoritos.map((f) => f.id)
@@ -26,13 +21,11 @@ const ProdutosComponent = ({
   return (
     <>
       <S.Produtos>
-        {produtos.map((produto) => (
+        {produtos?.map((produto) => (
           <Produto
             estaNosFavoritos={produtoEstaNosFavoritos(produto)}
             key={produto.id}
             produto={produto}
-            favoritar={favoritar}
-            aoComprar={adicionarAoCarrinho}
           />
         ))}
       </S.Produtos>
---
+++ teste.js
@@ -0,0 +1,5 @@
+const array = [{ x: 1 }, { x: 2 }, { x: 3 }]
+
+const rest = array.filter((element) => element.x > 1)
+
+console.log(rest)
